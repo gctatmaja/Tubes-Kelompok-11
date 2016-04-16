bb7550b30c379c0fae0faf08b9be2fc4aa18a814
Gctatmaja <genturcta@gmail.com>
2016-04-17 05:44:46 +0700

    UPDATE ASISTEN

---
+++ TUBES/src/tubes/Asisten.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+/**
+ *
+ * @author useR
+ */
+
+public class Asisten extends Orang {
+    
+    public Asisten(String nim, String nama) {
+        super(nim, nama, "asisten");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
